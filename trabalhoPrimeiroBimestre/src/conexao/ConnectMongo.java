db622210e32ee31d8b48ab1c8d9d6bcceb4bff99
yuri vilela <yuri.svilela@outlook.com.br>
2019-11-18 23:56:40 -0200

    drives

--- trabalhoPrimeiroBimestre/src/conexao/ConnectMongo.java
+++ trabalhoPrimeiroBimestre/src/conexao/ConnectMongo.java
@@ -1,30 +1,15 @@
 package conexao;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoDatabase;
+import dto.dto;
+import model.FunctionMongoDB;
+
+
 
 public class ConnectMongo {
 	public static void main( String args[] ){
-		try {
-
-MongoClientURI uri = new MongoClientURI(
-    "mongodb://yuri:20berv18@facu-shard-00-00-tym0q.gcp.mongodb.net:27017,facu-shard-00-01-tym0q.gcp.mongodb.net:27017,facu-shard-00-02-tym0q.gcp.mongodb.net:27017/test?ssl=true&replicaSet=facu-shard-0&authSource=admin&retryWrites=true&w=majority");
-
-MongoClient mongoClient = new MongoClient(uri);
-MongoDatabase database = mongoClient.getDatabase("prova2");
-
-MongoCollection<Document> collection = database.getCollection("endereco");
-
-Document doc = new Document();
-doc.put("rua", "sao tomas de aquino");
-doc.put("cep", "32215-240");
-
-collection.insertOne(doc);
-
-	}catch (Exception e) {
-		// TODO: handle exception
-		System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	}
+		dto dto = new dto();
+		FunctionMongoDB test = new FunctionMongoDB();
+		test.cadastrar(dto);
+		
 		}
 }
--- trabalhoPrimeiroBimestre/src/model/FunctionMongoDB.java
+++ trabalhoPrimeiroBimestre/src/model/FunctionMongoDB.java
@@ -1,10 +1,37 @@
 package model;
-
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import dto.dto;
 
-public class FunctionMongoDB {
 
+public class FunctionMongoDB {
+	MongoClientURI uri = new MongoClientURI(
+		    "mongodb://yuri:20berv18@facu-shard-00-00-tym0q.gcp.mongodb.net:27017,facu-shard-00-01-tym0q.gcp.mongodb.net:27017,facu-shard-00-02-tym0q.gcp.mongodb.net:27017/test?ssl=true&replicaSet=facu-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+			MongoClient mongoClient = new MongoClient(uri);
+			MongoDatabase database = mongoClient.getDatabase("prova2");
+			
+	public void cadastrar(dto dto) {
+		try {
+
+
+
+				MongoCollection<Document> collection = database.getCollection("endereco");
+
+				Document doc = new Document();
+				doc.put("rua", "sao tomas de aquino 4");
+				doc.put("cep", "casado");
+
+				collection.insertOne(doc);
+
+				}catch (Exception e) {
+					// TODO: handle exception
+					System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+				}
+	}
 }
+
