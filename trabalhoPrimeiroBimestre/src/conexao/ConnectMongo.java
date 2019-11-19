db764ce041c90f98e98891341dd7bf3ba713c3a0
yuri vilela <yuri.svilela@outlook.com.br>
2019-11-18 23:35:47 -0200

    conecta / cadastra no mongo

--- trabalhoPrimeiroBimestre/src/conexao/ConnectMongo.java
+++ trabalhoPrimeiroBimestre/src/conexao/ConnectMongo.java
@@ -1,21 +1,37 @@
 package conexao;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.*;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
 import org.bson.Document;
-import static com.mongodb.client.model.Filters.*;
-import org.bson.types.ObjectId;
+
+
 
 public class ConnectMongo {
+	public static void main( String args[] ){
+		try {
 
-	private MongoClient mongoClient;
-	private MongoDatabase database;
-	private MongoCollection<Document> collection;
-	
-	public ConexaoMongoDB() {
-		mongoClient = new MongoClient("localhost",27017);
-		database = mongoClient.getDatabase("");
-		cllection = database.getCollection("enderecos");
+MongoClientURI uri = new MongoClientURI(
+    "mongodb://yuri:20berv18@facu-shard-00-00-tym0q.gcp.mongodb.net:27017,facu-shard-00-01-tym0q.gcp.mongodb.net:27017,facu-shard-00-02-tym0q.gcp.mongodb.net:27017/test?ssl=true&replicaSet=facu-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+MongoClient mongoClient = new MongoClient(uri);
+MongoDatabase database = mongoClient.getDatabase("prova2");
+
+MongoCollection<Document> collection = database.getCollection("endereco");
+
+Document doc = new Document();
+doc.put("rua", "sao tomas de aquino");
+doc.put("cep", "32215-240");
+
+collection.insertOne(doc);
+
+	}catch (Exception e) {
+		// TODO: handle exception
+		System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 	}
-	
+		}
 }
